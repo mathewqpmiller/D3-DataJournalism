588e20091a18c137d07cc11564b8908ab6f97c6c
mathewqpmiller <mathewpqmiller@gmail.com>
2021-04-19 13:41:36 -0700

    final repo file structure, src pathed html

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,10 @@
-// @TODO: YOUR CODE HERE!
+// JavaScript code for Homework Assignment number 13: D3 Animated Scatter Plot
+
+// Scatter Plot Container Box Parameters
+let width = parseInt(d3.select("#scatter").style("width"));
+let height = width - width / 4.0;
+let margin = 20;
+let labelArea = 110;
+let tPadBot = 40;
+let tPadLeft = 40;
+
